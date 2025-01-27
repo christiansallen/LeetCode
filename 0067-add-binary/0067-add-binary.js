ba4ac52c033a1c8aa4964c193caa965c70bd6efb
Christian Allen <christiansallen@gmail.com>
2025-01-27 11:16:52 -0700

    Time: 2 ms (48.82%), Space: 52 MB (12.88%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+  let i = a.length - 1;
+  let j = b.length - 1;
+  let carry = 0;
+  let result = [];
+
+  while (i >= 0 || j >= 0 || carry > 0) {
+    const digitA = i >= 0 ? parseInt(a[i]) : 0;
+    const digitB = j >= 0 ? parseInt(b[j]) : 0;
+    const sum = digitA + digitB + carry;
+    result.push(sum % 2);
+    carry = Math.floor(sum / 2);
+
+    i--;
+    j--;
+  }
+
+  return result.reverse().join("");
+};
